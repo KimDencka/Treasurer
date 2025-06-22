110de371d82ef95cf676e9f03d1f212a9fefc6c8
Denis Kim <densungkim@gmail.com>
2025-06-22 22:39:12 +0500

    Refactor TransactionRequest and TransactionType; remove unused JSON encoding/decoding logic

--- src/main/scala/com/densungkim/treasurer/model/transaction/TransactionType.scala
+++ src/main/scala/com/densungkim/treasurer/model/transaction/TransactionType.scala
@@ -2,16 +2,10 @@ package com.densungkim.treasurer.model.transaction
 
 import com.densungkim.treasurer.util.JsonUtils
 import enumeratum._
-//import io.circe.{Decoder, Encoder}
 
 sealed trait TransactionType extends EnumEntry
 object TransactionType       extends Enum[TransactionType] with CirceEnum[TransactionType] with JsonUtils {
   val values: IndexedSeq[TransactionType] = findValues
   case object INCOME  extends TransactionType
   case object EXPENSE extends TransactionType
-
-//  implicit val encoder: Encoder[TransactionType] =
-//    Encoder.forProduct1("type")(_.entryName)
-//  implicit val decoder: Decoder[TransactionType] =
-//    Decoder.instance(_.downField("type").as[String].map(TransactionType.withName))
 }
