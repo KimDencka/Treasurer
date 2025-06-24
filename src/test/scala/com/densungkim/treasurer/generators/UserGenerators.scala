8aaef22b20a3327015c5811abd59665549b05aa2
Denis Kim <densungkim@gmail.com>
2025-06-25 01:47:11 +0500

    Add Unit Test For AuthService

--- src/test/scala/com/densungkim/treasurer/generators/UserGenerators.scala
+++ src/test/scala/com/densungkim/treasurer/generators/UserGenerators.scala
@@ -1,3 +1,24 @@
-package com.densungkim.treasurer.generators class UserGenerators {
+package com.densungkim.treasurer.generators
+
+import com.densungkim.treasurer.model.user._
+import org.scalacheck.Gen
+
+trait UserGenerators extends CommonGenerators {
+
+  def genUserRequest: Gen[UserRequest] =
+    for {
+      username <- genRefinedNEString
+      password <- genRefinedNEString
+    } yield UserRequest(username, password)
+
+  def genUser: Gen[User] =
+    for {
+      id        <- Gen.uuid
+      username  <- genString(15)
+      password  <- genString(15).map(PasswordHash.apply)
+      createdAt <- genLocalDateTime
+    } yield User(id, username, password, createdAt)
+
+  def genUserResponse: Gen[UserResponse] = genUser.map(_.toResponse)
 
 }
