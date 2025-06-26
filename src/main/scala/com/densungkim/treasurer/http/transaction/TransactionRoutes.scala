facb2eb832f26b9ded7a42377484bfa15cf040c6
Denis Kim <densungkim@gmail.com>
2025-06-26 21:58:02 +0500

    Add integration tests for TransactionRoutes

--- src/main/scala/com/densungkim/treasurer/http/transaction/TransactionRoutes.scala
+++ src/main/scala/com/densungkim/treasurer/http/transaction/TransactionRoutes.scala
@@ -5,6 +5,7 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import com.densungkim.treasurer.http.auth.AuthDirectives
 import com.densungkim.treasurer.http.rejectionHandler
+import com.densungkim.treasurer.model.ErrorModels.TransactionNotFound
 import com.densungkim.treasurer.model.transaction.{TransactionRequest, TransactionType}
 import com.densungkim.treasurer.service.{AuthService, TransactionService}
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
@@ -43,39 +44,51 @@ final class TransactionRoutes(
           } ~
             path("type" / Segment) { transactionTypeStr =>
               get {
-                onComplete(transactionService.getByType(userId, TransactionType.withName(transactionTypeStr))) {
-                  case Success(transactions) =>
-                    complete(StatusCodes.OK -> transactions)
-                  case Failure(e)            =>
-                    complete(StatusCodes.BadRequest -> e.getMessage)
+                if (TransactionType.values.map(_.entryName).contains(transactionTypeStr)) {
+                  onComplete(transactionService.getByType(userId, TransactionType.withName(transactionTypeStr))) {
+                    case Success(transactions) =>
+                      complete(StatusCodes.OK -> transactions)
+                    case Failure(e)            =>
+                      complete(StatusCodes.InternalServerError -> e.getMessage)
+                  }
+                } else {
+                  complete(
+                    StatusCodes.BadRequest -> s"InvalidType is not a member of Enum (${TransactionType.values.mkString(",")})",
+                  )
                 }
               }
             } ~
             path(JavaUUID) { id =>
               get {
                 onComplete(transactionService.getById(id, userId)) {
-                  case Success(transactionResponse) =>
+                  case Success(transactionResponse)    =>
                     complete(StatusCodes.OK -> transactionResponse)
-                  case Failure(e)                   =>
-                    complete(StatusCodes.NotFound -> e.getMessage)
+                  case Failure(e: TransactionNotFound) =>
+                    complete(StatusCodes.NotFound -> e.message)
+                  case Failure(e)                      =>
+                    complete(StatusCodes.InternalServerError -> e.getMessage)
                 }
               } ~
                 put {
                   entity(as[TransactionRequest]) { request =>
                     onComplete(transactionService.update(id, userId, request)) {
-                      case Success(transactionResponse) =>
+                      case Success(transactionResponse)    =>
                         complete(StatusCodes.OK -> transactionResponse)
-                      case Failure(e)                   =>
-                        complete(StatusCodes.NotFound -> e.getMessage)
+                      case Failure(e: TransactionNotFound) =>
+                        complete(StatusCodes.NotFound -> e.message)
+                      case Failure(e)                      =>
+                        complete(StatusCodes.InternalServerError -> e.getMessage)
                     }
                   }
                 } ~
                 delete {
                   onComplete(transactionService.delete(id, userId)) {
-                    case Success(_) =>
+                    case Success(_)                      =>
                       complete(StatusCodes.NoContent)
-                    case Failure(e) =>
-                      complete(StatusCodes.NotFound -> e.getMessage)
+                    case Failure(e: TransactionNotFound) =>
+                      complete(StatusCodes.NotFound -> e.message)
+                    case Failure(e)                      =>
+                      complete(StatusCodes.InternalServerError -> e.getMessage)
                   }
                 }
             }
--- src/main/scala/com/densungkim/treasurer/model/ErrorModels.scala
+++ src/main/scala/com/densungkim/treasurer/model/ErrorModels.scala
@@ -3,8 +3,9 @@ package com.densungkim.treasurer.model
 import scala.util.control.NoStackTrace
 
 object ErrorModels {
-  final case class CryptoException(message: String)     extends NoStackTrace
-  final case class UserNotFound(message: String)        extends NoStackTrace
-  final case class IncorrectPassword(message: String)   extends NoStackTrace
-  final case class TransactionNotFound(message: String) extends NoStackTrace
+  final case class CryptoException(message: String)          extends NoStackTrace
+  final case class UserNotFound(message: String)             extends NoStackTrace
+  final case class IncorrectPassword(message: String)        extends NoStackTrace
+  final case class TransactionNotFound(message: String)      extends NoStackTrace
+  final case class IncorrectTransactionType(message: String) extends NoStackTrace
 }
